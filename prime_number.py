4b644b920f8f9889c996a09f63d4ca745264569f
Raveena <raveena>
2015-07-31 12:57:02 +0530

    second commit

--- prime_number.py
+++ prime_number.py
@@ -3,7 +3,7 @@ upper = int(input("Enter upper range: "))
 
 for num in range(lower,upper + 1):
    if num > 1:
-       for i in range(2,num):
+   for i in range(2,num):
            if (num % i) == 0:
                break
        else:
